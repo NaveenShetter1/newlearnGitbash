a9ad072fb55372ebc8b4bbe87bcaf800a707181a
Ubuntu <ubuntu@ip-172-31-46-128.eu-north-1.compute.internal>
2025-03-20 19:26:35 +0000

    add

--- ml_script.py
+++ ml_script.py
@@ -1,1 +1,9 @@
-print("This is testing script .yml file"
+#e ML model (Modify as needed)
+X = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 4, 6, 8, 10])
+
+print("Training Data:")
+print(X, y)
+
+print("ML script executed successfully on EC2!")
+
